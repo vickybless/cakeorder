6ad990b90ce157ef9e8acd185a8451227e417f6c
victoria <victoriachiamaka08@gmail.com>
2022-03-09 15:37:32 +0100

    updating order button

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -8,7 +8,7 @@ const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 
-  const hasItems = cartCtx.items.lenght > 0;
+  const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = (id) => {};
   //this is to Add items to the cart.
